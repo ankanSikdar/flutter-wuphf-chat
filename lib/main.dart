80d450828ef265a7e9fdd387c7d83cecad9117fa
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-08 13:49:53 +0530

    Added text theme and google fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wuphf_chat/screens/signup/signup_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,8 +18,26 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlueAccent[300],
         accentColor: Colors.black,
         scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme().copyWith(
+          headline3: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.w900,
+            color: Colors.black,
+          ),
+        ),
+        fontFamily: GoogleFonts.nunitoSans().fontFamily,
       ),
-      home: Scaffold(),
+      initialRoute: SignUpScreen.routeName,
+      routes: {
+        SignUpScreen.routeName: (context) => SignUpScreen(),
+      },
     );
   }
 }
+// 
+
+// TextStyle(
+//               fontSize: 48.0,
+//               fontWeight: FontWeight.w900,
+//               fontFamily: GoogleFonts.nunitoSans().fontFamily,
+//             ),
