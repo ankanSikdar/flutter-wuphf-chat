71b71debd146c4a51b6b05886e6a71c98ecfce68
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-10 00:48:33 +0530

    Added firebase sdk to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wuphf_chat/config/config.dart';
 import 'screens/screens.dart';
 
-void main() {
+Future<void> main() async {
+  /*
+   The WidgetFlutterBinding is used to interact with the Flutter engine. Firebase.initializeApp() needs to call native code to initialize Firebase, and since the plugin needs to use platform channels to call the native code, which is done asynchronously therefore you have to call ensureInitialized() to make sure that you have an instance of the WidgetsBinding.
+  */
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
