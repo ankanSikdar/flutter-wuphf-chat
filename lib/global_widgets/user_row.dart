cfcf2e025e5503918364954ac18b0e00f89b6bfc
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-11 01:04:17 +0530

    Created user row widget

---
+++ lib/global_widgets/user_row.dart
@@ -0,0 +1,59 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class UserRow extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imageUrl;
+
+  const UserRow({
+    Key key,
+    @required this.title,
+    @required this.subtitle,
+    @required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(35.0)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(35.0),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Container(color: Colors.grey),
+              errorWidget: (context, url, error) =>
+                  Container(color: Colors.grey),
+            ),
+          ),
+        ),
+        SizedBox(width: 16.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            SizedBox(height: 4.0),
+            Text(
+              subtitle,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
