8d24fcec3f342e4882f92f03e52eb655b87cc871
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-12 15:51:54 +0530

    Added ontap to user row

--- lib/global_widgets/user_row.dart
+++ lib/global_widgets/user_row.dart
@@ -5,12 +5,16 @@ class UserRow extends StatelessWidget {
   final String title;
   final String subtitle;
   final String imageUrl;
+  final Function onChat;
+  final Function onView;
 
   const UserRow({
     Key key,
     @required this.title,
     @required this.subtitle,
     @required this.imageUrl,
+    this.onChat,
+    this.onView,
   }) : super(key: key);
 
   @override
@@ -19,42 +23,48 @@ class UserRow extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
         children: [
-          Container(
-            height: 70,
-            width: 70,
-            decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(35.0)),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(35.0),
-              child: CachedNetworkImage(
-                imageUrl: imageUrl,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => Container(color: Colors.grey),
-                errorWidget: (context, url, error) =>
-                    Container(color: Colors.grey),
+          GestureDetector(
+            onTap: onView,
+            child: Container(
+              height: 70,
+              width: 70,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(35.0)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(35.0),
+                child: CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Container(color: Colors.grey),
+                  errorWidget: (context, url, error) =>
+                      Container(color: Colors.grey),
+                ),
               ),
             ),
           ),
           SizedBox(width: 16.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
+          GestureDetector(
+            onTap: onChat,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-              ),
-              SizedBox(height: 4.0),
-              Text(
-                subtitle,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
+                SizedBox(height: 4.0),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
