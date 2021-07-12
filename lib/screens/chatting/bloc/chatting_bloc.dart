c37047cc12a323334d06c5fe5e5e8cbde028313a
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-13 00:52:54 +0530

    Added comments and on close function

--- lib/screens/chatting/bloc/chatting_bloc.dart
+++ lib/screens/chatting/bloc/chatting_bloc.dart
@@ -24,6 +24,12 @@ class ChattingBloc extends Bloc<ChattingEvent, ChattingState> {
   }
 
   @override
+  Future<void> close() {
+    _messagesSubscription.cancel();
+    super.close();
+  }
+
+  @override
   Stream<ChattingState> mapEventToState(
     ChattingEvent event,
   ) async* {
@@ -41,23 +47,29 @@ class ChattingBloc extends Bloc<ChattingEvent, ChattingState> {
     }
   }
 
+  // Check if user has messaged this person before
   Stream<ChattingState> _mapCheckHasMessagedBeforeToState() async* {
     try {
       if (state.messagesDbRef != null) {
         // Navigated from chatScreen
+        // Therefore user must have messaged this person before
+        // And messagesDbRef is present so we can fetch all the messages
         yield (state.copyWith(hasMessagedBefore: true));
         add(ChattingFetchMessages());
       } else {
         // Navigated from usersScreen
+        // Therefore we have to check to see if user has messaged this person before
         final check =
             await _messagesRepository.checkMessagesExists(user: state.user);
         if (check == true) {
+          // User has messaged this person before
           final messagesDb = await _messagesRepository
               .getAlreadyPresentMessagesDb(user: state.user);
           yield (state.copyWith(
               messagesDbRef: messagesDb, hasMessagedBefore: true));
           add(ChattingFetchMessages());
         } else {
+          // User has no messages with this person
           yield (state.copyWith(hasMessagedBefore: false));
         }
       }
@@ -66,18 +78,24 @@ class ChattingBloc extends Bloc<ChattingEvent, ChattingState> {
     }
   }
 
+  // Sending a new message
   Stream<ChattingState> _mapSendMessageToState(
       ChattingSendMessage event) async* {
     try {
       if (state.hasMessagedBefore) {
+        // User already has a messageDb with this person therefore we just add the message to it
         await _messagesRepository.sendMessage(
           recipientId: state.user.id,
           documentReference: state.messagesDbRef,
           message: event.message,
         );
       } else {
+        // User has no messageDb with this person
+        // We create the db and send the first message
         final messagesDbRef = await _messagesRepository.sendFirstMessage(
             user: state.user, message: event.message);
+
+        // Now hasMessagedBefore must be set to true and we also add the messagesDb to state
         yield (state.copyWith(
             hasMessagedBefore: true, messagesDbRef: messagesDbRef));
         add(ChattingFetchMessages());
@@ -87,12 +105,15 @@ class ChattingBloc extends Bloc<ChattingEvent, ChattingState> {
     }
   }
 
+  // Getting the stream of messages
   Stream<ChattingState> _mapFetchMessagesToState() async* {
     try {
       yield (state.copyWith(status: ChattingStatus.loading));
       if (state.hasMessagedBefore == false) {
+        // Double check to ensure user has messaged this person before and messageDb exists
         yield (state.copyWith(status: ChattingStatus.loaded));
       } else {
+        // Creating a new messagesSubscription and canceling any old one present
         _messagesSubscription?.cancel();
         _messagesSubscription = _messagesRepository
             .getMessagesList(messagesDbRef: state.messagesDbRef)
@@ -106,8 +127,10 @@ class ChattingBloc extends Bloc<ChattingEvent, ChattingState> {
     }
   }
 
+  // Updating messagesList
   Stream<ChattingState> _mapUpdateMessagesToState(
       ChattingUpdateMessages event) async* {
+    // Adding the messagesList with the new messageList
     yield (state.copyWith(messagesList: event.messagesList));
   }
 }
