61f21b789f6244b61a243d280f024dabaf66f773
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-13 14:33:32 +0530

    Created message widget

---
+++ lib/screens/chatting/widgets/message_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:wuphf_chat/models/models.dart';
+
+class MessageWidget extends StatelessWidget {
+  final Message message;
+  final bool isAuthor;
+
+  const MessageWidget({
+    Key key,
+    @required this.message,
+    @required this.isAuthor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final marginSide = MediaQuery.of(context).size.width * 0.3;
+    return Container(
+      margin: isAuthor
+          ? EdgeInsets.fromLTRB(marginSide, 0.0, 8.0, 16.0)
+          : EdgeInsets.fromLTRB(8.0, 0.0, marginSide, 16.0),
+      padding: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[350],
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(message.text),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('12/01/99'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
