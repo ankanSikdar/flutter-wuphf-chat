e4e47aa34eb0d2c159571d643ce6e4b69410b1a5
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-14 16:22:36 +0530

    Partially designed user profile page

--- lib/screens/user_profile/user_profile_screen.dart
+++ lib/screens/user_profile/user_profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wuphf_chat/bloc/blocs.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wuphf_chat/repositories/user/user_repository.dart';
+import 'package:wuphf_chat/screens/user_profile/widgets/widgets.dart';
 
 class UserProfileScreen extends StatelessWidget {
   static const String routeName = '/user-profile-screen';
@@ -10,16 +12,30 @@ class UserProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              context.read<AuthBloc>().add(AuthUserLogOut());
-            },
-          ),
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            UserAppBar(),
+          ];
+        },
+        body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            UserHeader(
+              title: 'About',
+            ),
+            AboutContainer(),
+            UserHeader(
+              title: 'Settings',
+            ),
+            ElevatedButton.icon(
+                icon: Icon(Icons.exit_to_app),
+                label: Text('Sign Out'),
+                onPressed: () {
+                  context.read<AuthBloc>().add(AuthUserLogOut());
+                })
+          ],
+        ),
       ),
     );
   }
