7dc2802a454a3d48f1b329968558803bbd4b6430
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-14 19:46:50 +0530

    Created SignOut Button

---
+++ lib/screens/user_profile/widgets/signout_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:wuphf_chat/bloc/blocs.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignOutButton extends StatelessWidget {
+  const SignOutButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<AuthBloc>().add(AuthUserLogOut());
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          color: Colors.grey[200],
+        ),
+        margin: EdgeInsets.symmetric(vertical: 8.0),
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.exit_to_app),
+              SizedBox(width: 8.0),
+              Text('Sign Out'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
