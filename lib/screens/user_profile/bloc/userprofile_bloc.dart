d580634022226276bf91c9ab8e42717626020945
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-14 20:53:39 +0530

    Replaced firebaseauth with userId String

--- lib/screens/user_profile/bloc/userprofile_bloc.dart
+++ lib/screens/user_profile/bloc/userprofile_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/foundation.dart';
 import 'package:wuphf_chat/models/models.dart';
 import 'package:wuphf_chat/repositories/repositories.dart';
@@ -11,15 +10,17 @@ part 'userprofile_event.dart';
 part 'userprofile_state.dart';
 
 class UserProfileBloc extends Bloc<UserProfileEvent, UserProfileState> {
+  final String _userId;
   final UserRepository _userRepository;
-  final firebase_auth.FirebaseAuth _firebaseAuth;
 
   UserProfileBloc({
     @required UserRepository userRepository,
-    @required firebase_auth.FirebaseAuth firebaseAuth,
-  })  : _userRepository = userRepository,
-        _firebaseAuth = firebaseAuth,
-        super(UserProfileState.initial());
+    @required String userId,
+  })  : _userId = userId,
+        _userRepository = userRepository,
+        super(UserProfileState.initial()) {
+    add(LoadUserProfile());
+  }
 
   @override
   Stream<UserProfileState> mapEventToState(
@@ -34,8 +35,7 @@ class UserProfileBloc extends Bloc<UserProfileEvent, UserProfileState> {
     yield (state.copyWith(status: UserProfileStatus.loading));
 
     try {
-      final userId = _firebaseAuth.currentUser.uid;
-      final user = await _userRepository.getUserWithId(userId: userId);
+      final user = await _userRepository.getUserWithId(userId: _userId);
       yield (state.copyWith(user: user, status: UserProfileStatus.loaded));
     } catch (e) {
       yield (state.copyWith(status: UserProfileStatus.error, error: e.message));
