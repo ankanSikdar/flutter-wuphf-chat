3c69012011318e49f60d32a013d4401327835c18
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-16 21:11:27 +0530

    Added inkwell to go to view profile screen

--- lib/screens/chatting/widgets/chatting_app_bar.dart
+++ lib/screens/chatting/widgets/chatting_app_bar.dart
@@ -21,51 +21,60 @@ class ChattingAppBar extends StatelessWidget {
       automaticallyImplyLeading: false,
       leadingWidth: 35.0,
       titleSpacing: 0.0,
-      title: Container(
-        child: Row(
-          children: [
-            SizedBox(width: 8.0),
-            Container(
-              height: 75,
-              width: 75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35.0),
-                color: Colors.grey[200],
-              ),
-              padding: EdgeInsets.all(3.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(35.0),
-                child: CachedNetworkImage(
-                  imageUrl: user.profileImageUrl,
-                  fit: BoxFit.cover,
-                  placeholder: (context, url) => Container(color: Colors.grey),
-                  errorWidget: (context, url, error) =>
-                      Container(color: Colors.grey),
+      title: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            ViewProfileScreen.routeName,
+            arguments: ViewProfileScreenArgs(user: user),
+          );
+        },
+        child: Container(
+          child: Row(
+            children: [
+              SizedBox(width: 8.0),
+              Container(
+                height: 75,
+                width: 75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35.0),
+                  color: Colors.grey[200],
+                ),
+                padding: EdgeInsets.all(3.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(35.0),
+                  child: CachedNetworkImage(
+                    imageUrl: user.profileImageUrl,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) =>
+                        Container(color: Colors.grey),
+                    errorWidget: (context, url, error) =>
+                        Container(color: Colors.grey),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 16.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    user.displayName,
-                    style: Theme.of(context).textTheme.headline5,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    user.email,
-                    style: Theme.of(context).textTheme.subtitle1,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+              SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user.displayName,
+                      style: Theme.of(context).textTheme.headline5,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      user.email,
+                      style: Theme.of(context).textTheme.subtitle1,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       leading: InkWell(
