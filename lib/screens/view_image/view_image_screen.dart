91a78fc895ea26425d744e41f7c8003eb920e561
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-19 20:46:49 +0530

    Added interactive viewer for zooming image

--- lib/screens/view_image/view_image_screen.dart
+++ lib/screens/view_image/view_image_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:wuphf_chat/global_widgets/global_widgets.dart';
 
 class ViewImageScreenArgs {
   final String imageUrl;
@@ -22,8 +24,24 @@ class ViewImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(imageUrl),
+      body: CustomScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        slivers: [
+          TwoTextAppBar(title: 'View Image', subtitle: 'Pinch to zoom'),
+          SliverFillRemaining(
+            child: InteractiveViewer(
+              minScale: 0.5,
+              maxScale: 3,
+              child: CachedNetworkImage(
+                imageUrl: imageUrl,
+                fit: BoxFit.fitWidth,
+                placeholder: (context, url) => Container(color: Colors.grey),
+                errorWidget: (context, url, error) =>
+                    Container(color: Colors.grey),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
