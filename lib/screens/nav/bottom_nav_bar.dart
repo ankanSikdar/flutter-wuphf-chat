3c8a308b6b5ebc1c7ab22d3fceddc2a9142c4dc7
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-25 13:03:17 +0530

    Commiting the same thing for 3rd time

--- lib/screens/nav/bottom_nav_bar.dart
+++ lib/screens/nav/bottom_nav_bar.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -64,7 +63,6 @@ class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
       onWillPop: () async => false,
       child: Scaffold(
         body: _screens.elementAt(_selectedIndex),
-<<<<<<< HEAD
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.vertical(
@@ -98,55 +96,6 @@ class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
               onTap: _onItemTapped,
             ),
           ),
-||||||| 15bede5
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: [
-            BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.users), label: 'Contacts'),
-            BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.comments), label: 'Chat'),
-            BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.userCircle), label: 'Profile'),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-=======
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(15.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0, -2),
-                blurRadius: 4.0,
-                color: Theme.of(context).accentColor.withOpacity(0.2),
-              ),
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(15.0),
-            ),
-            child: BottomNavigationBar(
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              items: [
-                BottomNavigationBarItem(
-                    icon: FaIcon(FontAwesomeIcons.users), label: 'Contacts'),
-                BottomNavigationBarItem(
-                    icon: FaIcon(FontAwesomeIcons.comments), label: 'Chat'),
-                BottomNavigationBarItem(
-                    icon: FaIcon(FontAwesomeIcons.userCircle),
-                    label: 'Profile'),
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            ),
-          ),
->>>>>>> 5e88f8f8ccea9a578f683938b6b92fc046a835f4
         ),
       ),
     );
