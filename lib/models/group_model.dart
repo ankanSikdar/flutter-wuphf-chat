2d9dd1da498a264829893768350305141be531c8
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-27 20:35:30 +0530

    Created group model class

---
+++ lib/models/group_model.dart
@@ -0,0 +1,67 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:wuphf_chat/models/message_model.dart';
+
+class Group extends Equatable {
+  final String groupId;
+  final String groupName;
+  final String groupImage;
+  final Message lastMessage;
+  final List<String> participants;
+
+  Group({
+    @required this.groupId,
+    @required this.groupName,
+    @required this.groupImage,
+    @required this.lastMessage,
+    @required this.participants,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'groupId': groupId,
+      'groupName': groupName,
+      'groupImage': groupImage,
+      'lastMessage': lastMessage.toDocument(),
+      'participants': participants,
+    };
+  }
+
+  factory Group.fromMap(Map<String, dynamic> map) {
+    return Group(
+      groupId: map['groupId'],
+      groupName: map['groupName'],
+      groupImage: map['groupImage'],
+      lastMessage: Message.fromMap(map),
+      participants: List<String>.from(map['participants']),
+    );
+  }
+
+  @override
+  List<Object> get props {
+    return [
+      groupId,
+      groupName,
+      groupImage,
+      lastMessage,
+      participants,
+    ];
+  }
+
+  Group copyWith({
+    String groupId,
+    String groupName,
+    String groupImage,
+    Message lastMessage,
+    List<String> participants,
+  }) {
+    return Group(
+      groupId: groupId ?? this.groupId,
+      groupName: groupName ?? this.groupName,
+      groupImage: groupImage ?? this.groupImage,
+      lastMessage: lastMessage ?? this.lastMessage,
+      participants: participants ?? this.participants,
+    );
+  }
+}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,3 +2,4 @@ export 'user_model.dart';
 export 'chat_user_model.dart';
 export 'message_model.dart';
 export 'chats_list.dart';
+export 'group_model.dart';
