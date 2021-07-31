a5f4efffea7ef9a2c2226832b6f29d85d1299b53
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-31 21:31:38 +0530

    Provided CreateGroupCubit and BlocConsumer

--- lib/screens/create_group/create_group_screen.dart
+++ lib/screens/create_group/create_group_screen.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wuphf_chat/global_widgets/global_widgets.dart';
 import 'package:wuphf_chat/models/models.dart';
+import 'package:wuphf_chat/repositories/repositories.dart';
 import 'package:wuphf_chat/screens/create_group/widgets/group_name_widget.dart';
+import 'package:wuphf_chat/screens/create_group/widgets/participants_widget.dart';
 import 'package:wuphf_chat/screens/edit_profile/widgets/widgets.dart';
+
+import 'cubit/creategroup_cubit.dart';
 
 class CreateGroupScreenArgs {
   final List<User> participants;
@@ -18,73 +23,91 @@ class CreateGroupScreen extends StatefulWidget {
   static Route route({@required CreateGroupScreenArgs args}) {
     return MaterialPageRoute(
       settings: RouteSettings(name: routeName),
-      builder: (context) => CreateGroupScreen(participants: args.participants),
+      builder: (context) => BlocProvider(
+        create: (context) => CreateGroupCubit(
+          groupsRepository: context.read<GroupsRepository>(),
+          storageRepository: context.read<StorageRepository>(),
+          participants: args.participants,
+        ),
+        child: CreateGroupScreen(),
+      ),
     );
   }
 
-  final List<User> participants;
-
-  const CreateGroupScreen({Key key, @required this.participants})
-      : super(key: key);
+  const CreateGroupScreen({Key key}) : super(key: key);
 
   @override
   _CreateGroupScreenState createState() => _CreateGroupScreenState();
 }
 
 class _CreateGroupScreenState extends State<CreateGroupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           TwoTextAppBar(title: 'Create Group', subtitle: 'Edit Group Details'),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Form(
-                child: Column(
-                  children: [
-                    ChangeProfilePicture(
-                      imageUrl:
-                          'https://figeit.com/images/chat/mck-icon-group.png',
-                      onChanged: (File file) {},
+          BlocConsumer<CreateGroupCubit, CreateGroupState>(
+            listener: (context, state) {
+              if (state.status == CreateGroupStatus.error) {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(content: Text('${state.error}')),
+                  );
+                context.read<CreateGroupCubit>().reset();
+              }
+              if (state.status == CreateGroupStatus.submitting) {
+                _formKey.currentState.deactivate();
+              }
+              if (state.status == CreateGroupStatus.success) {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(content: Text('Group Created!')),
+                  );
+              }
+            },
+            builder: (context, state) {
+              return SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ChangeProfilePicture(
+                          imageUrl:
+                              'https://figeit.com/images/chat/mck-icon-group.png',
+                          onChanged: context
+                              .read<CreateGroupCubit>()
+                              .groupImageChanged,
+                        ),
+                        SizedBox(height: 16.0),
+                        GroupNameWidget(
+                          onChanged:
+                              context.read<CreateGroupCubit>().groupNameChanged,
+                        ),
+                        SizedBox(height: 16.0),
+                        InkWellButton(
+                          onTap: () {
+                            print('Check: ${_formKey.currentState.validate()}');
+                          },
+                          buttonColor: Theme.of(context).primaryColor,
+                          title: 'Create Group',
+                          titleColor: Colors.white,
+                        ),
+                        SizedBox(height: 16.0),
+                        ParticipantsWidget(participants: state.participants),
+                      ],
                     ),
-                    SizedBox(height: 16.0),
-                    GroupNameWidget(onChanged: (value) {}),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.all(16.0),
-            sliver: SliverToBoxAdapter(
-              child: InkWellButton(
-                onTap: () {},
-                buttonColor: Theme.of(context).primaryColor,
-                title: 'Create Group',
-                titleColor: Colors.white,
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-              child: InputTitle(title: 'Participants'),
-            ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                final user = widget.participants[index];
-                return UserRow(
-                  imageUrl: user.profileImageUrl,
-                  title: user.displayName,
-                  subtitle: user.bio,
-                );
-              }, childCount: widget.participants.length),
-            ),
+              );
+            },
           ),
         ],
       ),
