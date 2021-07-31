b2b7fd797ee259a2545623aa37b6c5184ac83295
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-31 21:04:52 +0530

    Created submitForm and uploadGroupImage

--- lib/screens/create_group/cubit/creategroup_cubit.dart
+++ lib/screens/create_group/cubit/creategroup_cubit.dart
@@ -11,8 +11,6 @@ import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 part 'creategroup_state.dart';
 
 class CreateGroupCubit extends Cubit<CreateGroupState> {
-  final firebase_auth.FirebaseAuth _firebaseAuth;
-  final UserRepository _userRepository;
   final StorageRepository _storageRepository;
   final GroupsRepository _groupsRepository;
 
@@ -22,10 +20,8 @@ class CreateGroupCubit extends Cubit<CreateGroupState> {
     @required StorageRepository storageRepository,
     @required GroupsRepository groupsRepository,
     firebase_auth.FirebaseAuth firebaseAuth,
-  })  : _userRepository = userRepository,
-        _storageRepository = storageRepository,
+  })  : _storageRepository = storageRepository,
         _groupsRepository = groupsRepository,
-        _firebaseAuth = firebase_auth.FirebaseAuth.instance,
         super(CreateGroupState.initial(participants: participants));
 
   Future<void> groupNameChanged(String value) {
@@ -36,32 +32,40 @@ class CreateGroupCubit extends Cubit<CreateGroupState> {
     emit(state.copyWith(newGroupImage: file));
   }
 
-  // Future<String> _uploadGroupImage({@required File file}) async {
-  //   try {
-  //     final downloadUrl = await _storageRepository.uploadProfileImage(
-  //       userId: _user.id,
-  //       file: file,
-  //     );
-  //     return downloadUrl;
-  //   } catch (e) {
-  //     throw Exception(e.message);
-  //   }
-  // }
+  Future<String> _uploadGroupImage({@required File file}) async {
+    try {
+      final downloadUrl = await _storageRepository.uploadGroupImage(
+        file: file,
+      );
+      return downloadUrl;
+    } catch (e) {
+      throw Exception(e.message);
+    }
+  }
 
-  // Future<void> submitForm() async {
-  //   emit(state.copyWith(status: CreateGroupStatus.submitting));
-  //   try {
-  //     if (state.newGroupImage != null) {
-  //       final newGroupDPImageUrl =
-  //           await _uploadGroupImage(file: state.newGroupImage);
-  //       emit(state.copyWith(groupImageUrl: newGroupDPImageUrl));
-  //     }
+  Future<void> submitForm() async {
+    emit(state.copyWith(status: CreateGroupStatus.submitting));
+    try {
+      if (state.newGroupImage != null) {
+        final newGroupDPImageUrl =
+            await _uploadGroupImage(file: state.newGroupImage);
+        emit(state.copyWith(groupImageUrl: newGroupDPImageUrl));
+      }
 
-  //     emit(state.copyWith(status: CreateGroupStatus.success));
-  //   } catch (e) {
-  //     emit(state.copyWith(status: CreateGroupStatus.error, error: e.message));
-  //   }
-  // }
+      final List<String> participantIds =
+          state.participants.map((user) => user.id).toList();
+
+      await _groupsRepository.createGroup(
+        groupName: state.groupName,
+        groupImageUrl: state.groupImageUrl,
+        participants: participantIds,
+      );
+
+      emit(state.copyWith(status: CreateGroupStatus.success));
+    } catch (e) {
+      emit(state.copyWith(status: CreateGroupStatus.error, error: e.message));
+    }
+  }
 
   void reset() {
     // Default Image and Participants are not reset
