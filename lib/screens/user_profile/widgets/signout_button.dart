6c387cc6d660d5d23adecd0e9197f02127c848da
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-18 00:55:40 +0530

    Replaced gesture detector with elevated button

--- lib/screens/user_profile/widgets/signout_button.dart
+++ lib/screens/user_profile/widgets/signout_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wuphf_chat/bloc/blocs.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wuphf_chat/config/configs.dart';
@@ -8,26 +9,36 @@ class SignOutButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        context.read<AuthBloc>().add(AuthUserLogOut());
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(ThemeConfig.borderRadius),
-          color: Colors.grey[200],
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton.icon(
+        onPressed: () {
+          context.read<AuthBloc>().add(AuthUserLogOut());
+        },
+        icon: FaIcon(
+          FontAwesomeIcons.signOutAlt,
+          color: Colors.black,
         ),
-        margin: EdgeInsets.symmetric(vertical: 8.0),
-        child: Padding(
-          padding: EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.exit_to_app),
-              SizedBox(width: 8.0),
-              Text('Sign Out'),
-            ],
+        label: Text(
+          'Sign Out',
+          style: TextStyle(color: Colors.black),
+        ),
+        style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all<Size>(
+            Size(MediaQuery.of(context).size.width * 0.8, 50.0),
           ),
+          elevation: MaterialStateProperty.all<double>(4.0),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            EdgeInsets.all(16.0),
+          ),
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(ThemeConfig.borderRadius),
+              ),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[200]),
         ),
       ),
     );
