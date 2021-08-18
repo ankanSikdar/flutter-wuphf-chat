a8afc6ca082327cfbb0002aa52cb3f56f354364a
Ankan Sikdar <ankansikdar@gmail.com>
2021-08-18 14:43:12 +0530

    Added sending email button when state is submitting

--- lib/screens/signup/forgot_password_screen.dart
+++ lib/screens/signup/forgot_password_screen.dart
@@ -41,19 +41,13 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       return;
     }
 
-    ScaffoldMessenger.of(context)
-      ..hideCurrentSnackBar()
-      ..showSnackBar(
-        SnackBar(content: Text('Checking...')),
-      );
-
     await context.read<SignUpCubit>().sendPasswordResetMail();
 
     if (context.read<SignUpCubit>().state.status != SignUpStatus.error) {
       ScaffoldMessenger.of(context)
         ..hideCurrentSnackBar()
         ..showSnackBar(
-          SnackBar(content: Text('Check your mail to reset password')),
+          SnackBar(content: Text('Check your email to reset password')),
         );
       _reset();
       Navigator.of(context).pop();
@@ -75,51 +69,75 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       },
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0.0,
-                        MediaQuery.of(context).size.height * 0.1, 0.0, 32.0),
-                    child: Text(
-                      'Send Mail',
-                      style: Theme.of(context).textTheme.headline3.apply(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                    ),
-                  ),
-                  EmailWidget(
-                    onChanged: context.read<SignUpCubit>().emailChanged,
-                  ),
-                  SizedBox(height: 32.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: BlocBuilder<SignUpCubit, SignUpState>(
+            builder: (context, state) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CustomElevatedButton(
-                        onTap: _onSubmit,
-                        titleColor: Theme.of(context).primaryColor,
-                        title: 'Send Code',
-                        buttonColor: Colors.white,
-                        icon: FontAwesomeIcons.fileImport,
+                      Container(
+                        margin: EdgeInsets.fromLTRB(
+                            0.0,
+                            MediaQuery.of(context).size.height * 0.1,
+                            0.0,
+                            32.0),
+                        child: Text(
+                          'Send Mail',
+                          style: Theme.of(context).textTheme.headline3.apply(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                        ),
                       ),
-                      CustomElevatedButton(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                        titleColor: Colors.grey,
-                        title: 'Go Back',
-                        buttonColor: Colors.white,
-                        icon: FontAwesomeIcons.chevronLeft,
-                      )
+                      EmailWidget(
+                        onChanged: context.read<SignUpCubit>().emailChanged,
+                      ),
+                      SizedBox(height: 32.0),
+                      state.status == SignUpStatus.submitting
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                CustomElevatedButton(
+                                  onTap: null,
+                                  titleColor: Colors.grey,
+                                  title: 'Sending Email',
+                                  buttonColor: Colors.white,
+                                  icon: FontAwesomeIcons.spinner,
+                                  size: Size(
+                                      MediaQuery.of(context).size.width * 0.6,
+                                      50.0),
+                                ),
+                              ],
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                CustomElevatedButton(
+                                  onTap: _onSubmit,
+                                  titleColor: Theme.of(context).primaryColor,
+                                  title: 'Send Code',
+                                  buttonColor: Colors.white,
+                                  icon: FontAwesomeIcons.fileImport,
+                                ),
+                                CustomElevatedButton(
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  titleColor: Colors.grey,
+                                  title: 'Go Back',
+                                  buttonColor: Colors.white,
+                                  icon: FontAwesomeIcons.chevronLeft,
+                                )
+                              ],
+                            ),
                     ],
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
         ),
       ),
